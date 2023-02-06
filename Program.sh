e2c02a7516c957d3e1d2d51f8a54876d2257d7fb
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-02-06 00:32:39 -0500

    A+B*C Operation

--- Program.sh
+++ Program.sh
@@ -2,3 +2,5 @@ echo "Welcome to Arithmetic Computation & sorting"
 read -p "Enter the first value:A " a;
 read -p "Enter the Second Value:B " b;
 read -p "Enter the third value:C " c;
+z=$(("$a+$b*$c"));
+printf "$a+$b*$c=$z";
